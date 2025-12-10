81fe957d1418a8ec2c04d941b930c4cf60108624
Sibin Jacob <sibinjacob169@gmail.com>
2025-12-10 22:35:45 +0530

    updates

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,12 @@ module.exports = function(eleventyConfig) {
   // Copy CSS files directly if you add a separate css folder later
   eleventyConfig.addPassthroughCopy("src/css");
 
+  // Filter to strip a leading slash so links can be used as relative paths
+  eleventyConfig.addFilter("stripLeadingSlash", function(str) {
+    if (!str || typeof str !== 'string') return str;
+    return str.charAt(0) === '/' ? str.slice(1) : str;
+  });
+
   return {
     dir: {
       input: "src",
